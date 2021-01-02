d5c0f4c56f601a1e4876a590b600e55e94ce62bb
shun-shun123 <shunsuke15vb@gmail.com>
2021-01-02 21:29:27 +0900

    feat: CustomText.csのスクリプトファイル作成

---
+++ Editor/CustomTextEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine.UI;
+
+namespace Mummy.CustomUI
+{
+    /// <summary>
+    /// CustomText Editor
+    /// </summary>
+    [CustomEditor(typeof(CustomText))]
+    public class CustomTextEditor : Editor
+    {
+        public void CreateCustomText()
+        {
+            
+        }
+    }
+}
---
+++ Runtime/CustomText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mummy.CustomUI
+{
+    /// <summary>
+    /// CustomText
+    /// </summary>
+    [AddComponentMenu("CustomUI/CustomText")]
+    public class CustomText : Text
+    {
+        /// <summary>
+        /// FontType
+        /// </summary>
+        public enum FontType
+        {
+            Normal,
+            Bold,
+            Warn,
+            Attention,
+            Special,
+        }
+    }
+}
