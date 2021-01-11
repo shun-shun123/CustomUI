d2c628c5975d4c854031e3900f1c5bcadc3c36b3
shun-shun123 <shunsuke15vb@gmail.com>
2021-01-11 19:27:19 +0900

    feat: CustomTextの実装完了

--- Runtime/CustomText.cs
+++ Runtime/CustomText.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Mummy.CustomUI
@@ -6,13 +7,13 @@ namespace Mummy.CustomUI
     /// <summary>
     /// CustomText
     /// </summary>
-    [AddComponentMenu("CustomUI/CustomText")]
+    [AddComponentMenu("CustomUI/CustomText", order:20)]
     public class CustomText : Text
     {
         /// <summary>
         /// FontType
         /// </summary>
-        public enum FontType
+        public enum TextType
         {
             Normal,
             Bold,
@@ -20,5 +21,61 @@ namespace Mummy.CustomUI
             Attention,
             Special,
         }
+
+        /// <summary>
+        /// 文字のタイプ
+        /// </summary>
+        [SerializeField]
+        private TextType textType;
+
+        /// <summary>
+        /// Awake
+        /// </summary>
+        protected override void Awake()
+        {
+            base.Awake();
+            if (Application.isPlaying)
+            {
+                SetupCustomText();
+            }
+            else
+            {
+                SetupCustomTextInEditorMode();
+            }
+        }
+
+        /// <summary>
+        /// Setup CustomText parameter according to textType
+        /// </summary>
+        private void SetupCustomText()
+        {
+            if (WordManager.Instance == null)
+            {
+                if (Application.isPlaying)
+                {
+                    Debug.LogError("WordManager is not found. You need to place one WordManager.");
+                }
+                else
+                {
+                    Debug.LogWarning("WordManager is not found. You need to place one WordManager.");
+                }
+                return;
+            }
+            var textTypeData = WordManager.Instance.GetTextTypeData(textType);
+            font = textTypeData.FontData;
+            fontSize = textTypeData.TextSize;
+            color = textTypeData.TextColor;
+        }
+        
+        #if UNITY_EDITOR
+        private void SetupCustomTextInEditorMode()
+        {
+            WordManager.Instance.LoadWordsData();
+            var textTypeData = WordManager.Instance.GetTextTypeData(textType);
+            font = textTypeData.FontData;
+            fontSize = textTypeData.TextSize;
+            color = textTypeData.TextColor;
+        }
+        #endif
     }
 }
---
+++ Runtime/Model/TextTypeData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mummy.CustomUI.Model
+{
+    /// <summary>
+    /// CustomText data
+    /// </summary>
+    public class TextTypeData
+    {
+        public readonly Font FontData;
+
+        public readonly int TextSize;
+
+        public readonly Color TextColor;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="fontData">FontData</param>
+        /// <param name="textSize">TextSize</param>
+        /// <param name="textColor">TextColor</param>
+        public TextTypeData(Font fontData, int textSize, Color textColor)
+        {
+            FontData = fontData;
+            TextSize = textSize;
+            TextColor = textColor;
+        }
+    }
+}
