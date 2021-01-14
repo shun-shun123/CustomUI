e84041df0ffc149e573a5b157d6b5efc75f81d30
shun-shun123 <shunsuke15vb@gmail.com>
2021-01-14 20:58:18 +0900

    feat: LongPressイベントを検知できるCustomButtonの実装

---
+++ Runtime/CustomButton.cs
@@ -0,0 +1,66 @@
+﻿using Mummy.CustomUI.Module;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Mummy.CustomUI
+{
+    /// <summary>
+    /// CustomButton
+    /// </summary>
+    [AddComponentMenu("CustomUI/CustomButton", 20)]
+    public class CustomButton : Button
+    {
+        /// <summary>
+        /// longPressHandler
+        /// </summary>
+        [SerializeField]
+        private LongPressHandler longPressHandler;
+
+        /// <summary>
+        /// isPressingButton flag
+        /// </summary>
+        private bool _isPressingButton;
+
+        /// <summary>
+        /// LongPressHandler
+        /// </summary>
+        public LongPressHandler.OnLongPressed OnLongPressed
+        {
+            get => longPressHandler.OnLongPressedHandler;
+            set => longPressHandler.OnLongPressedHandler = value;
+        }
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+            // check for mouse control
+            if (eventData.button != PointerEventData.InputButton.Left)
+            {
+                return;
+            }
+
+            _isPressingButton = true;
+        }
+
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            base.OnPointerUp(eventData);
+            // check for mouse control
+            if (eventData.button != PointerEventData.InputButton.Left)
+            {
+                return;
+            }
+
+            _isPressingButton = false;
+        }
+
+        private void Update()
+        {
+            if (_isPressingButton)
+            {
+                longPressHandler.CountDownLongPress();   
+            }
+        }
+    }
+}
